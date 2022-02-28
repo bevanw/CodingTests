2a6c7bf71ae8d189b05b81dec0e3ab6aefab7cf1
Bevan Wheeler <bevan.wheeler@more4appsnzltd.onmicrosoft.com>
2022-02-28 20:17:33 +1300

    Added initial drop table code.

---
+++ CodingTests.Tests/DropTable.Tests.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Fractions;
+using NUnit.Framework;
+
+namespace CodingTests.Tests
+{
+    [TestFixture]
+    public class DropTableTests
+    {
+        [Test]
+        public void GenerateDrop_Should_ListAllDropRates()
+        {
+            double rolls = 1000;
+
+            var table = new DropTableTests<string>();
+            //table.AddDrop("Legend Sword", new Fraction(1, 1000));
+            //table.AddDrop("Rare Sword", new Fraction(1, 100));
+            //table.AddDrop("Great Sword", new Fraction(1, 50));
+            //table.AddDrop("Sword", new Fraction(1, 25));
+            //table.AddDrop("Trash Sword", new Fraction(1, 10));
+            //table.AddBlankDrop();
+
+            table.AddDrop("L Sword", new Fraction(1, 10));
+            table.AddDrop("R Sword", new Fraction(1, 10));
+            table.AddBlankDrop();
+
+            var loot = new List<string>();
+            for (int i = 0; i < rolls; i++)
+            {
+                var drop = table.GenerateDrop();
+                loot.Add(drop);
+            }
+
+            Console.WriteLine($"Drop Table ({rolls:N0} rolls)");
+            Console.WriteLine($"----------------------------");
+            Console.WriteLine($"Item\t\tDrops\tRate");
+            foreach (var item in loot.GroupBy(l => l).Select(g => new{ Drop = g.Key, Count = g.Count()}).OrderBy(g => g.Count))
+            {
+                Console.WriteLine($"{item.Drop ?? "Blanks"}\t\t{item.Count}\t{Fraction.FromDoubleRounded(item.Count / rolls)}");
+            }
+        }
+
+        [Test]
+        public void GenerateDrop_Should_ReturnAmountOfDropsBasedOnWeight()
+        {
+            double rolls = 1000;
+            double expectedPercentage = 50;
+
+            var table = new DropTableTests<string>();
+            table.AddDrop("Item1", 50);
+            table.AddDrop("Item2", 50);
+
+            var loot = new List<string>();
+            for (int i = 0; i < rolls; i++)
+            {
+                var drop = table.GenerateDrop();
+                loot.Add(drop);
+            }
+
+            int amountOfDrops = loot.Count(l => l == "Item2");
+            (amountOfDrops / rolls * 100).Should().Be(expectedPercentage);
+        }
+    }
+}
+
---
+++ CodingTests/DropTableTests.cs
@@ -0,0 +1,64 @@
+﻿
+using Fractions;
+
+namespace CodingTests
+{
+    public class DropTableTests<T>
+    {
+        private class Drop
+        {
+            public Drop(T item, double accumulatedWeight)
+            {
+                Item = item;
+                AccumulatedWeight = accumulatedWeight;
+            }
+
+            public double AccumulatedWeight;
+            public T Item;
+        }
+
+        private readonly List<Drop> entries = new();
+        private readonly Random rand = new();
+
+        private double accumulatedWeight;
+
+        /// <summary>
+        /// Adds a drop to the drop table.
+        /// </summary>
+        /// <param name="item">Item to drop.</param>
+        /// <param name="dropWeight">Drop weight as a fraction..</param>
+        public void AddDrop(T item, Fraction dropWeight)
+        {
+            accumulatedWeight += dropWeight.ToDouble();
+            entries.Add(new Drop(item, accumulatedWeight));
+        }
+
+        /// <summary>
+        /// Inserts a blank drop to fill up gaps.
+        /// </summary>
+        public void AddBlankDrop()
+        {
+            double weight = 1 - accumulatedWeight;
+            accumulatedWeight += weight;
+            entries.Add(new Drop(default, accumulatedWeight));
+        }
+
+        /// <summary>
+        /// Generates a drop based on the drop rate.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public T GenerateDrop()
+        {
+            double r = rand.NextDouble() * accumulatedWeight;
+
+            foreach (Drop entry in entries)
+            {
+                if (entry.AccumulatedWeight >= r)
+                    return entry.Item;
+            }
+
+            throw new InvalidOperationException("No entries added");
+        }
+    }
+}
